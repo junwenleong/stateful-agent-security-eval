d9c71ca9df46f0dc11ad0de1c93700b7435f2dd0
Jun Wen Leong <leongjunwen@gmail.com>
2026-06-23 10:28:59 +0800

    chore: bump qwq version test to N=10

--- scripts/test_qwq_version.py
+++ scripts/test_qwq_version.py
@@ -4,7 +4,7 @@ from src.runner.config_loader import load_config
 from src.runner.runner import ExperimentRunner
 
 config = load_config('experiments/configs/defense_factorial.yaml')
-config.runs_per_condition = 1
+config.runs_per_condition = 10
 config.results_path = 'results/qwq_ollama_version_test/results.jsonl'
 config.models = [m for m in config.models if 'qwq' in m.get('model_name','')]
 config.defenses = [d for d in config.defenses if d.get('name') == 'no_defense']
